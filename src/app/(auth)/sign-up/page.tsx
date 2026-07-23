dcc90f30b173ceea58fb5bccc6963a84b4472e9d
Abdihanad Mohamed <hanad757a@gmail.com>
2026-07-22 23:50:44 -0400

    05: authentication-socials

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,17 @@
-import { SignUpView } from "@/modules/auth/ui/views/sign-up-view"
+import { auth } from "@/lib/auth";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
 
-const SignUp = () => {
-    console.log("sign up page")
-    return <SignUpView/>
-}
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default SignUp
+const SignUp = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) redirect("/");
+
+  return <SignUpView />;
+};
+
+export default SignUp;
