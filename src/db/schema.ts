74f6557e2a52240602e3866dadfc0c369d325035
Abdihanad Mohamed <hanad757a@gmail.com>
2026-07-28 22:10:50 -0400

    09: agents setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -32,7 +33,6 @@ export const session = pgTable(
   },
   (table) => [index("session_userId_idx").on(table.userId)],
 );
-
 
 export const account = pgTable(
   "account",
@@ -92,3 +92,17 @@ export const accountRelations = relations(account, ({ one }) => ({
     references: [user.id],
   }),
 }));
+
+export const agents = pgTable("agents", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  name: text("name").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  instruction: text("instructions").notNull(),
+
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -7,8 +7,5 @@ import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Hanady" }));
-
-  return <div className="flex flex-col p-4 gap-y-4">{data?.greeting}</div>;
+  return <div>Home view</div>;
 };
